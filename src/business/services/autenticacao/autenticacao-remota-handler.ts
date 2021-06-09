712cebc5424c6b54c07cee5d2f12aa19d3d991fe
Rafael Batista <3909@ucdb.br>
2021-06-09 16:55:24 -0400

    feature: ensure remote authentication returns an AccountModel on sucess

--- src/business/services/autenticacao/autenticacao-remota-handler.ts
+++ src/business/services/autenticacao/autenticacao-remota-handler.ts
@@ -3,16 +3,17 @@ import { AutenticacaoParams } from "@/shared/types";
 import { IHttpPostServico } from "@/business/protocols/http-post-servico";
 import { EHttpStatusCode } from "@/shared/enums";
 import { AccontModel } from "@/domain/models/AccontModel";
+import { IAutenticacao } from "@/domain/casos-de-uso/autenticacao";
 
-export class AutenticacaoRemotaHandler {
+export class AutenticacaoRemotaHandler implements IAutenticacao {
 	constructor(private readonly url: string, private readonly httpServico: IHttpPostServico<AutenticacaoParams, AccontModel>) {}
 
-	async handler(body: AutenticacaoParams): Promise<void> {
+	async handler(body: AutenticacaoParams): Promise<AccontModel> {
 		const response = await this.httpServico.post({ url: this.url, body });
 
 		switch (response.statusCode) {
 			case EHttpStatusCode.ok:
-				break;
+				return response.body;
 			case EHttpStatusCode.unauthorized:
 				throw new ErroCredenciaisInvalidas();
 			case EHttpStatusCode.badRequest:
--- src/tests/mocks/index.ts
+++ src/tests/mocks/index.ts
@@ -1,2 +1,2 @@
-export { mockBodyAutenticacao } from "./mock-autenticacao"
-export { MockHttpPostServicoSpy } from "./mock-http-servico"
+export { mockBodyAutenticacao, mockBodyAccountModel } from "./mock-autenticacao";
+export { MockHttpPostServicoSpy } from "./mock-http-servico";
