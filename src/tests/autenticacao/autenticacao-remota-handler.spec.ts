d7e244d5332c5588f8dae012d9fedf9b218436e5
Rafael Batista <3909@ucdb.br>
2021-06-09 16:10:17 -0400

    test: ensure remote authentication throws ErroCredenciaisInvalidas on 401

--- src/tests/autenticacao/autenticacao-remota-handler.spec.ts
+++ src/tests/autenticacao/autenticacao-remota-handler.spec.ts
@@ -1,37 +1,49 @@
-import { mockBodyAutenticacao, MockHttpPostServicoSpy } from '@/tests/mocks';
-import { AutenticacaoRemotaHandler } from '@/business/services/autenticacao/autenticacao-remota-handler';
+import { mockBodyAutenticacao, MockHttpPostServicoSpy } from "@/tests/mocks";
+import { AutenticacaoRemotaHandler } from "@/business/services/autenticacao/autenticacao-remota-handler";
+import { ErroCredenciaisInvalidas } from "@/shared/errors";
+import { EHttpStatusCode } from "@/shared/enums";
 import faker from "faker";
 
 type SUTTypes = {
-    sut: AutenticacaoRemotaHandler,
-    httpPostServicoSpy: MockHttpPostServicoSpy
-}
+	sut: AutenticacaoRemotaHandler;
+	httpPostServicoSpy: MockHttpPostServicoSpy;
+};
 
 const criarSUT = (url = faker.internet.url()): SUTTypes => {
-    const httpPostServicoSpy = new MockHttpPostServicoSpy();
-    const sut = new AutenticacaoRemotaHandler(url, httpPostServicoSpy);
+	const httpPostServicoSpy = new MockHttpPostServicoSpy();
+	const sut = new AutenticacaoRemotaHandler(url, httpPostServicoSpy);
 
-    return {
-        sut,
-        httpPostServicoSpy
-    }
-}
+	return {
+		sut,
+		httpPostServicoSpy,
+	};
+};
 
-describe('business.services.autenticacao', () => {
-    test('ao invovar handler | quando invocar httpServico | deve estar com url correta', async () => {
-        const url = faker.internet.url();
-        const {sut, httpPostServicoSpy}= criarSUT(url);
+describe("business.services.autenticacao", () => {
+	test("ao invovar handler | quando invocar httpServico | deve estar com url correta", async () => {
+		const url = faker.internet.url();
+		const { sut, httpPostServicoSpy } = criarSUT(url);
 
-        await sut.autenticar(mockBodyAutenticacao());
+		await sut.handler(mockBodyAutenticacao());
 
-        expect(httpPostServicoSpy.url).toBe(url);
-    });
+		expect(httpPostServicoSpy.url).toBe(url);
+	});
 
-    test('ao invovar handler | quando invocar httpServico | deve estar com body correto', async () => {
-        const {sut, httpPostServicoSpy}= criarSUT();
-        const autenticacoParams = mockBodyAutenticacao();
-        await sut.autenticar(autenticacoParams);
+	test("ao invovar handler | quando invocar httpServico | deve estar com body correto", async () => {
+		const { sut, httpPostServicoSpy } = criarSUT();
+		const autenticacoParams = mockBodyAutenticacao();
 
-        expect(httpPostServicoSpy.body).toEqual(autenticacoParams);
-    });
+		await sut.handler(autenticacoParams);
+
+		expect(httpPostServicoSpy.body).toEqual(autenticacoParams);
+	});
+
+	test("ao invovar handler | quando retorno httpPostServico é 401 | deve lançar ErroCredenciaisInvalidas", async () => {
+		const { sut, httpPostServicoSpy } = criarSUT();
+		httpPostServicoSpy.response.statusCode = EHttpStatusCode.unauthorized;
+
+		const promise = sut.handler(mockBodyAutenticacao());
+
+		await expect(promise).rejects.toThrow(new ErroCredenciaisInvalidas());
+	});
 });
--- src/tests/mocks/mock-http-servico.ts
+++ src/tests/mocks/mock-http-servico.ts
@@ -1,15 +1,18 @@
-import { HttpPostParams } from '@/shared/types';
-import { IHttpPostServico } from '@/business/protocols/http-post-servico';
+import { HttpPostParams, HttpResponse } from "@/shared/types";
+import { IHttpPostServico } from "@/business/protocols/http-post-servico";
+import { EHttpStatusCode } from "@/shared/enums";
 
-export class MockHttpPostServicoSpy implements IHttpPostServico{
-    url?:string
-    body?:object
+export class MockHttpPostServicoSpy implements IHttpPostServico {
+	url?: string;
+	body?: object;
+	response: HttpResponse = {
+		statusCode: EHttpStatusCode.noContent,
+	};
 
-    async post(params:HttpPostParams): Promise<void> {
-       this.url = params.url;
-       this.body = params.body;
+	async post(params: HttpPostParams): Promise<HttpResponse> {
+		this.url = params.url;
+		this.body = params.body;
 
-       return Promise.resolve();
-    }
-
+		return this.response;
+	}
 }
