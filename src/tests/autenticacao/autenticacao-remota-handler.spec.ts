da3b1000793e03b23757f063ee3be00fcb9461cb
Rafael Batista <3909@ucdb.br>
2021-06-09 14:18:36 -0400

    refactor: move sut creation to a factory method

--- src/tests/autenticacao/autenticacao-remota-handler.spec.ts
+++ src/tests/autenticacao/autenticacao-remota-handler.spec.ts
@@ -1,12 +1,26 @@
 import { HttpPostServicoSpy } from '../../mocks/http-servico';
 import { AutenticacaoRemotaHandler } from '../../business/services/autenticacao/autenticacao-remota-handler';
 
+type SUTTypes = {
+    sut: AutenticacaoRemotaHandler,
+    httpPostServicoSpy: HttpPostServicoSpy
+}
+
+const criarSUT = (url = "qualquer_url"): SUTTypes => {
+    const httpPostServicoSpy = new HttpPostServicoSpy();
+    const sut = new AutenticacaoRemotaHandler(url, httpPostServicoSpy);
+
+    return {
+        sut,
+        httpPostServicoSpy
+    }
+}
+
 describe('business.services.autenticacao', () => {
     test('ao invovar handler | quando invocar httpServico | deve estar com url correta', async () => {
+        const url = "outra_url";
+        const {sut, httpPostServicoSpy}= criarSUT(url);
 
-        const url = "qualquer_url";
-        const httpPostServicoSpy = new HttpPostServicoSpy();
-        const sut = new AutenticacaoRemotaHandler(url, httpPostServicoSpy);
         await sut.autenticar();
 
         expect(httpPostServicoSpy.url).toBe(url);
