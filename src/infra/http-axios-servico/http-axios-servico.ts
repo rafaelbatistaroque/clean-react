8f8156c1df1be5a64f2d5a026d0320a812b161b2
Rafael Batista <3909@ucdb.br>
2021-06-10 14:20:05 -0400

    feature: ensure httpServico calls axios with correct body

--- src/infra/http-axios-servico/http-axios-servico.ts
+++ src/infra/http-axios-servico/http-axios-servico.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export class HttpAxiosServico {
 	async post(params: HttpPostParams<any>): Promise<void> {
-		axios.post(params.url);
+		axios.post(params.url, params.body);
 	}
 }
