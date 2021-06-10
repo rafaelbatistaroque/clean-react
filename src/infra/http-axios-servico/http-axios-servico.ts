6991dc8b0e8cc865da32fc7c1f02b8597d0f2593
Rafael Batista <3909@ucdb.br>
2021-06-10 14:38:34 -0400

    feature: ensure httpServico return corrects values

--- src/infra/http-axios-servico/http-axios-servico.ts
+++ src/infra/http-axios-servico/http-axios-servico.ts
@@ -1,8 +1,14 @@
-import { HttpPostParams } from "@/shared/types";
+import { IHttpPostServico } from "@/business/protocols";
+import { HttpPostParams, HttpResponse } from "@/shared/types";
 import axios from "axios";
 
-export class HttpAxiosServico {
-	async post(params: HttpPostParams<any>): Promise<void> {
-		axios.post(params.url, params.body);
+export class HttpAxiosServico implements IHttpPostServico<any, any> {
+	async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+		const resposta = await axios.post(params.url, params.body);
+
+		return {
+			statusCode: resposta.status,
+			body: resposta.data,
+		};
 	}
 }
