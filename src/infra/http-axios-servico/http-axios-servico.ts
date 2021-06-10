f0a5840b40e7f52df6278bc53bfb5e1a117d7a43
Rafael Batista <3909@ucdb.br>
2021-06-10 13:20:46 -0400

    feature: ensure httpServico calls with correct url

---
+++ src/infra/http-axios-servico/http-axios-servico.ts
@@ -0,0 +1,8 @@
+import { HttpPostParams } from "@/shared/types";
+import axios from "axios";
+
+export class HttpAxiosServico {
+	async post(params: HttpPostParams<any>): Promise<void> {
+		axios(params.url);
+	}
+}
