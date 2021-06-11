02597bf9a069ea032c1ca54587e43cdb19ad9fe4
Rafael Batista <3909@ucdb.br>
2021-06-11 08:45:58 -0400

    chore: config scss webpack

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
         ".+\\.(ts|tsx)$": "ts-jest"
     },
     moduleNameMapper: {
-        "@/(.*)": "<rootDir>/src/$1"
+        "@/(.*)": "<rootDir>/src/$1",
+        "\\.scss$": "identity-obj-proxy"
     }
 }
